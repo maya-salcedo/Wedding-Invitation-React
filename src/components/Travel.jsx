ec7292a98466397beebb63c7e4ed4ac4a9352441
maya <maya.salcedo07@gmail.com>
2021-02-05 07:28:32 +0200

    fix styling of gift and travel page

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 const Travel = () => {
     return (
        <div>
-          <h2 className="information goldtextcss">Accommodation and Transportation</h2>
-
-    <p >You may stay anywhere in Como Lake area as you desire if you have a rented car
-        to move around. Varenna, 7 km north of Lierna, is a charming place to stay.
-        If you do not want to rent a car, Lecco City is a good place to find accommodation
-        as it has good train connections to Milan City and Malpensa Airport. Lecco City is
-        about 14 km south of Lierna. You can contact us for additional information.
-    </p>
+        <h2 className="information goldtextcss">Accommodation and Transportation</h2>
+        <p>You may stay anywhere in Como Lake area as you desire if you have a rented car to move around.</p>
+        <p>Varenna, 7 km north of Lierna, is a charming place to stay.</p>
+        <p>If you do not want to rent a car, Lecco City is a good place to find accommodation</p>
+        <p>as it has good train connections to Milan City and Malpensa Airport.</p>
+        <p>Lecco City is about 14 km south of Lierna.</p>
+        <p>You can contact us for additional information.</p>
        </div>
     );
 }
