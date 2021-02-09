28d1c6842958c4b4d9d7abdbbb04ae315ec8d2df
maya <maya.salcedo07@gmail.com>
2021-02-09 19:59:09 +0200

    style/home responsive

--- client/src/components/Wedding.jsx
+++ client/src/components/Wedding.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Flower from './image/flower.png';
+//import Flower from './image/flower.png';
+//<div className="flower-box"><img className="background-flower" src={Flower} alt="flower-img"/></div>
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCocktail, faUserTie } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios';
@@ -18,26 +19,26 @@ const Wedding = () => {
     }, []);
 
     return (
-       <div>
+       <div>      
        <div className="first-box">
-            <img className="background-flower" src={Flower} alt="flower-img"/>
+            
             <div className="centered">
-            <div>
-                <h3 className="invite">{instruction?.invite}</h3>
-                <h2 className="goldtextcss date"> {instruction?.date} </h2>
-                <h3 className="time">{instruction?.time}</h3>
-                <h3 className="goldtextcss church">{instruction?.church?.name}</h3>
-                <h4 className="address">{instruction?.church?.addressLine1}</h4>
-                <h4 className="address">{instruction?.church?.addressLine2}</h4>
+                <div>
+                    <h3 className="invite">{instruction?.invite}</h3>
+                    <h2 className="goldtextcss date"> {instruction?.date} </h2>
+                    <h3 className="time">{instruction?.time}</h3>
+                    <h3 className="goldtextcss church">{instruction?.church?.name}</h3>
+                    <h4 className="address">{instruction?.church?.addressLine1}</h4>
+                    <h4 className="address">{instruction?.church?.addressLine2}</h4>
+                </div>
+                <div>
+                    <p className="reception">Reception to follow at</p>
+                    <h3 className="goldtextcss restaurant">{instruction?.restaurant?.name}</h3>
+                    <h4 className="address">{instruction?.restaurant?.addressLine1}</h4>
+                    <h4 className="address">{instruction?.restaurant?.addressLine2}</h4></div>
+                </div>      
             </div>
-            <div>
-                <p className="reception">Reception to follow at</p>
-                <h3 className="goldtextcss restaurant">{instruction?.restaurant?.name}</h3>
-                <h4 className="address">{instruction?.restaurant?.addressLine1}</h4>
-                <h4 className="address">{instruction?.restaurant?.addressLine2}</h4></div>
-            </div>      
-        </div>
-        <div>
+        <div className="second-box">
             <h2 className="information">Wedding Information</h2>
             <div className="detail">
                 <FontAwesomeIcon icon={faCocktail} className="icon" />
