040648049b98ac4cc7f544ccd9e91d8c2a3d57da
marco <marco.castro@aller.com>
2021-02-12 14:34:16 +0200

    refactor: styled container to elements

---
+++ client/src/elements/StyledContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+display: grid;
+  text-align: center;
+  grid-template-columns: 1fr;
+`;
+
+const StyledContainer = ({ children }) => <StyledDiv>{children}</StyledDiv>;
+
+
+
+export default StyledContainer;
