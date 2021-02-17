67fe91dcab66bce569d0454f1372c60c416e6925
Marco-Ca <marcopaulocastro@ymail.com>
2021-02-17 22:55:25 +0200

    fix: client static

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-console.log(path.join(__dirname, '../client/build'))
-app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
