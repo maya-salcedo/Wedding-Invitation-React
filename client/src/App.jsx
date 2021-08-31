ab0d0a2f189edc6fa0fcc7c48762a32fa019e39b
maya <maya.salcedo07@gmail.com>
2021-08-31 08:41:10 +0300

    small fixing: unnecessary spacing removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './elements/GlobalStyle';
-
 import Home from './components/Home';
 import Wedding from './components/Wedding';
 import Gift from './components/Gift';
@@ -18,32 +17,31 @@ import ConfirmedDecline from './components/ConfirmedDecline';
 import UnconfirmedDecline from './components/UnconfirmedDecline';
 import { FlagContext } from './components/FlagContext';
 
-
 const App = () => {
-    const [flag, setFlag] = useState('english')
-    return (
-      <BrowserRouter>
-        <GlobalStyle />
-        <FlagContext.Provider value={{flag, setFlag}} >
-          <Navigation />
-          <Switch >
-            <Route path="/" component={Home} exact />
-            <Route path="/wedding" component={Wedding}/>
-            <Route path="/gift" component={Gift} />
-            <Route path="/travel" component={Travel} />
-            <Route path="/rsvp" component={Rsvp} />
-            <Route path="/accept" component={Accept} />
-            <Route path="/decline" component={Decline} />
-            <Route path="/confirmed-accept" component={ConfirmedAccept} />
-            <Route path="/unconfirmed-accept" component={UnconfirmedAccept} />
-            <Route path="/confirmed-decline" component={ConfirmedDecline} />
-            <Route path="/unconfirmed-decline" component={UnconfirmedDecline} />
-            <Route component={Error} />
-          </Switch>
-          <Footer />
-        </FlagContext.Provider>
-      </BrowserRouter>
-    );
-}
+  const [flag, setFlag] = useState('english');
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <FlagContext.Provider value={{ flag, setFlag }}>
+        <Navigation />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/wedding" component={Wedding} />
+          <Route path="/gift" component={Gift} />
+          <Route path="/travel" component={Travel} />
+          <Route path="/rsvp" component={Rsvp} />
+          <Route path="/accept" component={Accept} />
+          <Route path="/decline" component={Decline} />
+          <Route path="/confirmed-accept" component={ConfirmedAccept} />
+          <Route path="/unconfirmed-accept" component={UnconfirmedAccept} />
+          <Route path="/confirmed-decline" component={ConfirmedDecline} />
+          <Route path="/unconfirmed-decline" component={UnconfirmedDecline} />
+          <Route component={Error} />
+        </Switch>
+        <Footer />
+      </FlagContext.Provider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
